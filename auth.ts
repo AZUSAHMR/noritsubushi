ee6bfc395563e387ab2b2a0370339249be467c06
azu <azusahmr@k-on.kr>
2024-04-25 01:48:12 +0900

    feat: 로그인

---
+++ auth.ts
@@ -0,0 +1,30 @@
+import { AuthOptions } from "next-auth";
+import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
+
+export const authOptions: AuthOptions = {
+    providers: [
+        NaverProvider({
+            clientId: process.env.NAVER_CLIENT_ID!,
+            clientSecret: process.env.NAVER_CLIENT_SECRET!,
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID!,
+            clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+        }),
+    ],
+    callbacks: {
+        jwt({ token, account }) {
+            if (account) {
+                token.id = `${account.provider}:${token.sub}`;
+            }
+            return token;
+        },
+        session({ session, token }) {
+            if (token) {
+                session.user.id = token.id;
+            }
+            return session;
+        },
+    },
+};
